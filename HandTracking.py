c60410431fd4b7f2282a4410df29d1bd98043f0e
kimbro <95573884+kimpro12@users.noreply.github.com>
2023-09-23 21:22:00 +0700

    Create HandTracking.py

---
+++ HandTracking.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+
+class HandDetector:
+    def __init__(self, static_image_mode=False, max_num_hands=2, model_complexity=1,
+                 min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.static_image_mode = static_image_mode
+        self.max_num_hands = max_num_hands
+        self.model_complexity = model_complexity
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.static_image_mode, self.max_num_hands,
+                                        self.model_complexity, self.min_detection_confidence,
+                                        self.min_tracking_confidence)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.results = None
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=False):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, cord in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(cord.x * w), int(cord.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        return lmList
